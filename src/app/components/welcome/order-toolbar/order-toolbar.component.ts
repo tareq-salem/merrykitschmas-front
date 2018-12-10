478b4bdd090eb23716a908c48b2837aa921a62a7
Julien Laforge <julien.laforge@gmx.fr>
2018-12-10 14:36:12 +0100

    change radio buttons to buttons for ordering products

--- src/app/components/welcome/order-toolbar/order-toolbar.component.ts
+++ src/app/components/welcome/order-toolbar/order-toolbar.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-order-toolbar',
-  templateUrl: './order-toolbar.component.html',
-  styleUrls: ['./order-toolbar.component.css']
+    selector: 'app-order-toolbar',
+    templateUrl: './order-toolbar.component.html',
+    styleUrls: ['./order-toolbar.component.css']
 })
 export class OrderToolbarComponent implements OnInit {
 
-  constructor() { }
+    dateArrow = 'arrow_drop_down';
+    priceArrow = '';
+    constructor() { }
+    @Input() orderBy: string;
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    onDateClick() {
+        this.priceArrow = '';
+        if (this.dateArrow === 'arrow_drop_down') {
+            this.dateArrow = 'arrow_drop_up';
+            this.orderBy = 'recentDate';
+        } else {
+            this.dateArrow = 'arrow_drop_down';
+            this.orderBy = 'olderDate';
+        }
+        console.log(this.orderBy);
+    }
+
+    onPriceClick() {
+        this.dateArrow = '';
+        if (this.priceArrow === 'arrow_drop_down') {
+            this.priceArrow = 'arrow_drop_up';
+            this.orderBy = 'increasingPrice';
+        } else {
+            this.priceArrow = 'arrow_drop_down';
+            this.orderBy = 'decreasingPrice';
+        }
+        console.log(this.orderBy);
+    }
 
 }
